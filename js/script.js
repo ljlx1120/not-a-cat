a488ead03585824211c220e09a6a3e1576e08b0d
Xing Li and Zach Beecher <xing@>
2017-02-13 14:22:11 -0800

    not a cat

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+$(function(){
+
+  $("p").click(function(){
+
+  alert("this is a header.");
+
+  });
+
+  $(".btn1").click(function(){
+    $("h1").hide();
+  });
+  $(".btn2").click(function(){
+    $("h1").show();
+  });
+
+
+
+
+});
